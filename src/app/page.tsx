7981f077ad5a0f576a2a8dd65312332323d80ea9
hariperisetla <harinagasai.perisetla@gmail.com>
2024-05-10 15:37:12 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { MdMovieCreation } from "react-icons/md";
+
+export default function Home() {
+  return (
+    <main>
+      RecFlix
+      <MdMovieCreation className="text-[20rem] text-indigo-600" />
+    </main>
+  );
+}
